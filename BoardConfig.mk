42db728561bbafea0e44e421881470141e234a53
Cosmin Tanislav <demonsingur@gmail.com>
2022-02-20 19:21:09 +0200

    mondrian: Add support for udfps long press sensor
    Change-Id: I7196dce967c9adbc62bb5156ade749c92c7780a1

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -12,5 +12,8 @@ include vendor/xiaomi/mondrian/BoardConfigVendor.mk
 
 DEVICE_PATH := device/xiaomi/mondrian
 
+# Properties
+TARGET_VENDOR_PROP += $(DEVICE_PATH)/properties/vendor.prop
+
 # Screen density
 TARGET_SCREEN_DENSITY := 560
--- device.mk
+++ device.mk
@@ -17,6 +17,10 @@ PRODUCT_PACKAGES += \
     SystemUIResMondrian \
     WifiResMondrian
 
+# Sensors
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/sensors/hals.conf:$(TARGET_COPY_OUT_VENDOR)/etc/sensors/hals.conf
+
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH)
