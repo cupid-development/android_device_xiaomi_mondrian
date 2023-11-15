dd978cde6ff7d4cf66f28ea59897d1420eb45ebd
Arian <arian.kulmer@web.de>
2023-11-15 18:55:16 +0100

    mondrian: Fixup camera blobs
    Change-Id: Ia0eff58ddecd3de7dac78f344b4a0f74bdd88f46

--- extract-files.sh
+++ extract-files.sh
@@ -8,6 +8,12 @@
 
 function blob_fixup() {
     case "${1}" in
+        vendor/etc/camera/mondrian_enhance_motiontuning.xml|vendor/etc/camera/mondrian_motiontuning.xml)
+            sed -i 's/xml=version/xml version/g' "${2}"
+            ;;
+        vendor/etc/camera/pureView_parameter.xml)
+            sed -i 's/=\([0-9]\+\)>/="\1">/g' "${2}"
+            ;;
     esac
 }
 
