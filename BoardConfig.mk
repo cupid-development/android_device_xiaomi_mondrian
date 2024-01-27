08f5d0fc1128d803c105307d9012b2f0baba5464
Arian <arian.kulmer@web.de>
2024-01-27 11:10:12 +0100

    mondrian: Add regulator and haptics kernel modules
    Change-Id: Ib0dc985eea62282a239c58915b2c6c4b64d54dcb

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -12,6 +12,16 @@ include vendor/xiaomi/mondrian/BoardConfigVendor.mk
 
 DEVICE_PATH := device/xiaomi/mondrian
 
+# Kernel
+second_stage_modules := \
+	wl2866d.ko \
+	qcom-hv-haptics.ko
+
+BOARD_VENDOR_RAMDISK_RECOVERY_KERNEL_MODULES_LOAD += $(second_stage_modules)
+BOARD_VENDOR_KERNEL_MODULES_LOAD += $(second_stage_modules)
+
+BOOT_KERNEL_MODULES += $(second_stage_modules)
+
 # Properties
 TARGET_SYSTEM_PROP += $(DEVICE_PATH)/properties/system.prop
 
