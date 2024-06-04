7852a7c831e6d0da4a913f1d3388f1d9fdd18f16
Your Name <you@example.com>
2024-06-04 18:00:26 +0800

    mondrian: Add RisingOS stuff

--- lineage_mondrian.mk
+++ lineage_mondrian.mk
@@ -7,7 +7,7 @@
 # Inherit from mondrian device
 $(call inherit-product, device/xiaomi/mondrian/device.mk)
 
-# Inherit from common lineage configuration
+# Inherit from common RisingOS configuration
 TARGET_DISABLE_EPPE := true
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
@@ -21,10 +21,25 @@ PRODUCT_SYSTEM_NAME := mondrian
 PRODUCT_SYSTEM_DEVICE := mondrian
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="mondrian-user 14 UKQ1.230804.001 OS1.0.6.0.UMNCNXM release-keys" \
+    PRIVATE_BUILD_DESC="mondrian-user 14 UKQ1.230804.001 V816.0.5.0.UMNMIXM release-keys" \
     TARGET_DEVICE=$(PRODUCT_SYSTEM_DEVICE) \
     TARGET_PRODUCT=$(PRODUCT_SYSTEM_NAME)
 
-BUILD_FINGERPRINT := Redmi/mondrian/mondrian:14/UKQ1.230804.001/OS1.0.6.0.UMNCNXM:user/release-keys
+BUILD_FINGERPRINT := Redmi/mondrian/mondrian:14/UKQ1.230804.001/V816.0.5.0.UMNMIXM:user/release-keys
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
+
+# RisingOS stuff
+RISING_CHIPSET="Snapdragon 8+ Gen1" 
+RISING_MAINTAINER="Finish211(Finish)"
+RISING_PACKAGE_TYPE := GAPPS
+TARGET_ENABLE_BLUR := true
+PRODUCT_NO_CAMERA := false
+
+# RisingOS GMS build flags
+WITH_GMS := true
+TARGET_CORE_GMS := true
+TARGET_CORE_GMS_EXTRAS := false
+TARGET_DEFAULT_PIXEL_LAUNCHER := false
+TARGET_SUPPORTS_NEXT_GEN_ASSISTANT := true
+TARGET_SUPPORTS_GOOGLE_RECORDER := true
