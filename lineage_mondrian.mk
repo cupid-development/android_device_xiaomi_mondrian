bc18bdc9e9061b7e71cbb6a8f93d8ab9e674a860
Your Name <you@example.com>
2024-06-04 17:20:12 +0800

    mondrian:Update blobs from OS1.0.6.0.UMNCNXM

--- lineage_mondrian.mk
+++ lineage_mondrian.mk
@@ -14,17 +14,17 @@ $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 PRODUCT_NAME := lineage_mondrian
 PRODUCT_DEVICE := mondrian
 PRODUCT_MANUFACTURER := Xiaomi
-PRODUCT_BRAND := POCO
-PRODUCT_MODEL := 23013PC75G
+PRODUCT_BRAND := Redmi
+PRODUCT_MODEL := 23013RK75C
 
-PRODUCT_SYSTEM_NAME := mondrian_global
+PRODUCT_SYSTEM_NAME := mondrian
 PRODUCT_SYSTEM_DEVICE := mondrian
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="mondrian_global-user 14 UKQ1.230804.001 V816.0.5.0.UMNMIXM release-keys" \
+    PRIVATE_BUILD_DESC="mondrian-user 14 UKQ1.230804.001 OS1.0.6.0.UMNCNXM release-keys" \
     TARGET_DEVICE=$(PRODUCT_SYSTEM_DEVICE) \
     TARGET_PRODUCT=$(PRODUCT_SYSTEM_NAME)
 
-BUILD_FINGERPRINT := POCO/mondrian_global/mondrian:14/UKQ1.230804.001/V816.0.5.0.UMNMIXM:user/release-keys
+BUILD_FINGERPRINT := Redmi/mondrian/mondrian:14/UKQ1.230804.001/OS1.0.6.0.UMNCNXM:user/release-keys
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
