dfcca2115c3cb6ec2a42acd053095020a1f14f62
Arian <arian.kulmer@web.de>
2024-08-12 15:05:48 +0200

    mondrian: Get rid of 32 bit blobs
    Change-Id: Ia92a7aa8292b2d5aaca7075f6613bf65484b3f43

--- device.mk
+++ device.mk
@@ -23,7 +23,7 @@ PRODUCT_PACKAGES += \
 
 # Sensors
 PRODUCT_PACKAGES += \
-    sensors.xiaomi
+    sensors.xiaomi:64
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/sensors/hals.conf:$(TARGET_COPY_OUT_VENDOR)/etc/sensors/hals.conf
