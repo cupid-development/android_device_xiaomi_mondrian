b8ea5c1c2e20706f7796afd56c75f9ca4cbb5f98
Arian <arian.kulmer@web.de>
2024-08-14 13:59:44 +0200

    mondrian: Commonize sensors.xiaomi
    Change-Id: I5bd0cc3858677c292814e2dc72438864a29ffecb

--- device.mk
+++ device.mk
@@ -22,9 +22,6 @@ PRODUCT_PACKAGES += \
     WifiResMondrian
 
 # Sensors
-PRODUCT_PACKAGES += \
-    sensors.xiaomi:64
-
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/sensors/hals.conf:$(TARGET_COPY_OUT_VENDOR)/etc/sensors/hals.conf
 
