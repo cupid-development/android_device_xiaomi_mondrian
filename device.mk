72b89a2c584bdfcd2a827cba170e2614b9bed425
Arian <arian.kulmer@web.de>
2024-08-29 20:48:29 +0200

    mondrian: Move sensors hals.conf back to vendor repo
    sensors.xiaomi is added by common tree now
    
    Change-Id: I541b656304bfde478966c84115d94bc989742149

--- device.mk
+++ device.mk
@@ -24,10 +24,6 @@ PRODUCT_PACKAGES += \
     WifiResMondrian \
     WifiResMondrianCN
 
-# Sensors
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/sensors/hals.conf:$(TARGET_COPY_OUT_VENDOR)/etc/sensors/hals.conf
-
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH)
